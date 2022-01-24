d69c84eed1efce7b0ddd65e589ec104758e7bc50
UJJWAL SHARMA <ujjwalsharma191297@gmail.com>
2022-01-24 19:48:49 +0530

    Update Day_4_Longest Consecutive Sequence.py

--- Day_4_Longest Consecutive Sequence.py
+++ Day_4_Longest Consecutive Sequence.py
@@ -1,8 +1,23 @@
 # Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 
 # You must write an algorithm that runs in O(n) time.
+# O(n)
+def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+                return 0
+        hash_set = set(nums)
+        maxi=0
+        for num in hash_set:
+            if num-1 not in hash_set:
+                x=num
+                cnt=1
+                while x+1 in hash_set:
+                    x+=1
+                    cnt+=1
+                maxi=max(maxi, cnt)
+        return maxi
 
-
+# O(nlog(n))
 def longestConsecutive(self, nums: List[int]) -> int:
         nums.sort()
         maxi, cnt=0, 1
