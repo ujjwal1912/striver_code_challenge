feb960b458ee99ca141cfb4a0aa49bf23384cf06
UJJWAL SHARMA <ujjwalsharma191297@gmail.com>
2022-02-05 21:59:43 +0530

    Update Day_5_Middle of the Linked List.py

--- Day_5_Middle of the Linked List.py
+++ Day_5_Middle of the Linked List.py
@@ -1,3 +1,16 @@
 # Given the head of a singly linked list, return the middle node of the linked list.
 # If there are two middle nodes, return the second middle node.
 
+def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+  if not head:
+    return head
+  l = 1
+  curr=head
+  while curr:
+    l+=1
+    curr=curr.next 
+  l = (l+1)//2-1
+  while l:
+    l-=1
+    head = head.next
+  return head
